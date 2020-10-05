ccb51cf07d0c494008172a44f7b9226e320ae73b
Francesco Casadei Lelli <francesco.casadei.lelli@elfaelettronica.it>
2020-10-05 15:25:40 +0200

    v1.0.1.0 | Nuovo logo 【だー怨】

--- TokiDrift/Forms/Resoconto.Designer.vb
+++ TokiDrift/Forms/Resoconto.Designer.vb
@@ -22,64 +22,66 @@ Partial Class Resoconto
     'Non modificarla mediante l'editor del codice.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.dgv_Resoconto = New System.Windows.Forms.DataGridView()
-        Me.clm_IdPiatto = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.clm_DescPiatto = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.clm_Qnt = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        CType(Me.dgv_Resoconto, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'dgv_Resoconto
-        '
-        Me.dgv_Resoconto.AllowUserToAddRows = False
-        Me.dgv_Resoconto.AllowUserToDeleteRows = False
-        Me.dgv_Resoconto.AllowUserToResizeRows = False
-        Me.dgv_Resoconto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgv_Resoconto.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.clm_IdPiatto, Me.clm_DescPiatto, Me.clm_Qnt})
-        Me.dgv_Resoconto.Location = New System.Drawing.Point(1, 1)
-        Me.dgv_Resoconto.Name = "dgv_Resoconto"
-        Me.dgv_Resoconto.ReadOnly = True
-        Me.dgv_Resoconto.RowHeadersVisible = False
-        Me.dgv_Resoconto.Size = New System.Drawing.Size(377, 629)
-        Me.dgv_Resoconto.TabIndex = 0
-        '
-        'clm_IdPiatto
-        '
-        Me.clm_IdPiatto.HeaderText = "Numero piatto"
-        Me.clm_IdPiatto.Name = "clm_IdPiatto"
-        Me.clm_IdPiatto.ReadOnly = True
-        '
-        'clm_DescPiatto
-        '
-        Me.clm_DescPiatto.HeaderText = "Descrizione piatto"
-        Me.clm_DescPiatto.Name = "clm_DescPiatto"
-        Me.clm_DescPiatto.ReadOnly = True
-        Me.clm_DescPiatto.Width = 120
-        '
-        'clm_Qnt
-        '
-        Me.clm_Qnt.HeaderText = "Quantità selezionata"
-        Me.clm_Qnt.Name = "clm_Qnt"
-        Me.clm_Qnt.ReadOnly = True
-        Me.clm_Qnt.Width = 130
-        '
-        'Resoconto
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(381, 632)
-        Me.Controls.Add(Me.dgv_Resoconto)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
-        Me.MaximizeBox = False
-        Me.MinimizeBox = False
-        Me.Name = "Resoconto"
-        Me.Text = "Resoconto ordini"
-        CType(Me.dgv_Resoconto, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Resoconto))
+		Me.dgv_Resoconto = New System.Windows.Forms.DataGridView()
+		Me.clm_IdPiatto = New System.Windows.Forms.DataGridViewTextBoxColumn()
+		Me.clm_DescPiatto = New System.Windows.Forms.DataGridViewTextBoxColumn()
+		Me.clm_Qnt = New System.Windows.Forms.DataGridViewTextBoxColumn()
+		CType(Me.dgv_Resoconto, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'dgv_Resoconto
+		'
+		Me.dgv_Resoconto.AllowUserToAddRows = False
+		Me.dgv_Resoconto.AllowUserToDeleteRows = False
+		Me.dgv_Resoconto.AllowUserToResizeRows = False
+		Me.dgv_Resoconto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+		Me.dgv_Resoconto.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.clm_IdPiatto, Me.clm_DescPiatto, Me.clm_Qnt})
+		Me.dgv_Resoconto.Location = New System.Drawing.Point(1, 1)
+		Me.dgv_Resoconto.Name = "dgv_Resoconto"
+		Me.dgv_Resoconto.ReadOnly = True
+		Me.dgv_Resoconto.RowHeadersVisible = False
+		Me.dgv_Resoconto.Size = New System.Drawing.Size(377, 629)
+		Me.dgv_Resoconto.TabIndex = 0
+		'
+		'clm_IdPiatto
+		'
+		Me.clm_IdPiatto.HeaderText = "Numero piatto"
+		Me.clm_IdPiatto.Name = "clm_IdPiatto"
+		Me.clm_IdPiatto.ReadOnly = True
+		'
+		'clm_DescPiatto
+		'
+		Me.clm_DescPiatto.HeaderText = "Descrizione piatto"
+		Me.clm_DescPiatto.Name = "clm_DescPiatto"
+		Me.clm_DescPiatto.ReadOnly = True
+		Me.clm_DescPiatto.Width = 120
+		'
+		'clm_Qnt
+		'
+		Me.clm_Qnt.HeaderText = "Quantità selezionata"
+		Me.clm_Qnt.Name = "clm_Qnt"
+		Me.clm_Qnt.ReadOnly = True
+		Me.clm_Qnt.Width = 130
+		'
+		'Resoconto
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(381, 632)
+		Me.Controls.Add(Me.dgv_Resoconto)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.MaximizeBox = False
+		Me.MinimizeBox = False
+		Me.Name = "Resoconto"
+		Me.Text = "Resoconto ordini"
+		CType(Me.dgv_Resoconto, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
 
-    End Sub
+	End Sub
 
-    Friend WithEvents dgv_Resoconto As DataGridView
+	Friend WithEvents dgv_Resoconto As DataGridView
     Friend WithEvents clm_IdPiatto As DataGridViewTextBoxColumn
     Friend WithEvents clm_DescPiatto As DataGridViewTextBoxColumn
     Friend WithEvents clm_Qnt As DataGridViewTextBoxColumn
--- TokiDrift/Forms/StartScreen.Designer.vb
+++ TokiDrift/Forms/StartScreen.Designer.vb
@@ -22,88 +22,88 @@ Partial Class StartScreen
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
-    Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(StartScreen))
-    Me.BtnMenuPranzo = New System.Windows.Forms.Button()
-    Me.BtnMenuCena = New System.Windows.Forms.Button()
-    Me.TxtPassword = New System.Windows.Forms.TextBox()
-    Me.BtnCommandCenter = New System.Windows.Forms.Button()
-    Me.Label1 = New System.Windows.Forms.Label()
-    Me.SuspendLayout()
-    '
-    'BtnMenuPranzo
-    '
-    Me.BtnMenuPranzo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-    Me.BtnMenuPranzo.Location = New System.Drawing.Point(78, 12)
-    Me.BtnMenuPranzo.Name = "BtnMenuPranzo"
-    Me.BtnMenuPranzo.Size = New System.Drawing.Size(235, 30)
-    Me.BtnMenuPranzo.TabIndex = 0
-    Me.BtnMenuPranzo.Text = "♥ P R A N Z O ♥"
-    Me.BtnMenuPranzo.UseVisualStyleBackColor = True
-    '
-    'BtnMenuCena
-    '
-    Me.BtnMenuCena.Enabled = False
-    Me.BtnMenuCena.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-    Me.BtnMenuCena.Location = New System.Drawing.Point(78, 41)
-    Me.BtnMenuCena.Name = "BtnMenuCena"
-    Me.BtnMenuCena.Size = New System.Drawing.Size(235, 30)
-    Me.BtnMenuCena.TabIndex = 1
-    Me.BtnMenuCena.Text = "♥ C E N A ♥"
-    Me.BtnMenuCena.UseVisualStyleBackColor = True
-    '
-    'TxtPassword
-    '
-    Me.TxtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-    Me.TxtPassword.Location = New System.Drawing.Point(78, 213)
-    Me.TxtPassword.Name = "TxtPassword"
-    Me.TxtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(9829)
-    Me.TxtPassword.Size = New System.Drawing.Size(235, 20)
-    Me.TxtPassword.TabIndex = 2
-    '
-    'BtnCommandCenter
-    '
-    Me.BtnCommandCenter.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-    Me.BtnCommandCenter.Location = New System.Drawing.Point(78, 184)
-    Me.BtnCommandCenter.Name = "BtnCommandCenter"
-    Me.BtnCommandCenter.Size = New System.Drawing.Size(235, 30)
-    Me.BtnCommandCenter.TabIndex = 3
-    Me.BtnCommandCenter.Text = "指 令 セ ン タ ー"
-    Me.BtnCommandCenter.UseVisualStyleBackColor = True
-    '
-    'Label1
-    '
-    Me.Label1.AutoSize = True
-    Me.Label1.BackColor = System.Drawing.Color.MediumVioletRed
-    Me.Label1.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-    Me.Label1.ForeColor = System.Drawing.Color.Gold
-    Me.Label1.Location = New System.Drawing.Point(163, 74)
-    Me.Label1.Name = "Label1"
-    Me.Label1.Size = New System.Drawing.Size(63, 15)
-    Me.Label1.TabIndex = 4
-    Me.Label1.Text = "v1.0.0.0"
-    '
-    'StartScreen
-    '
-    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-    Me.BackgroundImage = Global.TokiDriftApplication.My.Resources.Resources.StartScreenBkg
-    Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-    Me.ClientSize = New System.Drawing.Size(394, 261)
-    Me.Controls.Add(Me.Label1)
-    Me.Controls.Add(Me.BtnCommandCenter)
-    Me.Controls.Add(Me.TxtPassword)
-    Me.Controls.Add(Me.BtnMenuCena)
-    Me.Controls.Add(Me.BtnMenuPranzo)
-    Me.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-    Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-    Me.Name = "StartScreen"
-    Me.Text = "(っ◔◡◔)っ 【 ﻿T O K I   D R I F T 】 (っ◔◡◔)っ"
-    Me.ResumeLayout(False)
-    Me.PerformLayout()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(StartScreen))
+		Me.BtnMenuPranzo = New System.Windows.Forms.Button()
+		Me.BtnMenuCena = New System.Windows.Forms.Button()
+		Me.TxtPassword = New System.Windows.Forms.TextBox()
+		Me.BtnCommandCenter = New System.Windows.Forms.Button()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.SuspendLayout()
+		'
+		'BtnMenuPranzo
+		'
+		Me.BtnMenuPranzo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.BtnMenuPranzo.Location = New System.Drawing.Point(78, 12)
+		Me.BtnMenuPranzo.Name = "BtnMenuPranzo"
+		Me.BtnMenuPranzo.Size = New System.Drawing.Size(235, 30)
+		Me.BtnMenuPranzo.TabIndex = 0
+		Me.BtnMenuPranzo.Text = "♥ P R A N Z O ♥"
+		Me.BtnMenuPranzo.UseVisualStyleBackColor = True
+		'
+		'BtnMenuCena
+		'
+		Me.BtnMenuCena.Enabled = False
+		Me.BtnMenuCena.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.BtnMenuCena.Location = New System.Drawing.Point(78, 41)
+		Me.BtnMenuCena.Name = "BtnMenuCena"
+		Me.BtnMenuCena.Size = New System.Drawing.Size(235, 30)
+		Me.BtnMenuCena.TabIndex = 1
+		Me.BtnMenuCena.Text = "♥ C E N A ♥"
+		Me.BtnMenuCena.UseVisualStyleBackColor = True
+		'
+		'TxtPassword
+		'
+		Me.TxtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.TxtPassword.Location = New System.Drawing.Point(78, 213)
+		Me.TxtPassword.Name = "TxtPassword"
+		Me.TxtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(9829)
+		Me.TxtPassword.Size = New System.Drawing.Size(235, 20)
+		Me.TxtPassword.TabIndex = 2
+		'
+		'BtnCommandCenter
+		'
+		Me.BtnCommandCenter.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.BtnCommandCenter.Location = New System.Drawing.Point(78, 184)
+		Me.BtnCommandCenter.Name = "BtnCommandCenter"
+		Me.BtnCommandCenter.Size = New System.Drawing.Size(235, 30)
+		Me.BtnCommandCenter.TabIndex = 3
+		Me.BtnCommandCenter.Text = "指 令 セ ン タ ー"
+		Me.BtnCommandCenter.UseVisualStyleBackColor = True
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.BackColor = System.Drawing.Color.MediumVioletRed
+		Me.Label1.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.ForeColor = System.Drawing.Color.Gold
+		Me.Label1.Location = New System.Drawing.Point(163, 74)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(63, 15)
+		Me.Label1.TabIndex = 4
+		Me.Label1.Text = "v1.0.0.0"
+		'
+		'StartScreen
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackgroundImage = Global.TokiDriftApplication.My.Resources.Resources.StartScreenBkg
+		Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.ClientSize = New System.Drawing.Size(394, 261)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.BtnCommandCenter)
+		Me.Controls.Add(Me.TxtPassword)
+		Me.Controls.Add(Me.BtnMenuCena)
+		Me.Controls.Add(Me.BtnMenuPranzo)
+		Me.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.Name = "StartScreen"
+		Me.Text = "(っ◔◡◔)っ 【 ﻿T O K I   D R I F T 】 (っ◔◡◔)っ"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
-  End Sub
+	End Sub
 
-  Friend WithEvents BtnMenuPranzo As Button
+	Friend WithEvents BtnMenuPranzo As Button
 	Friend WithEvents BtnMenuCena As Button
 	Friend WithEvents TxtPassword As TextBox
 	Friend WithEvents BtnCommandCenter As Button
