b3b39eb6bb9805d31e11b2583c24dbed225afb20
TheRegRob <55835974+TheRegRob@users.noreply.github.com>
2021-05-10 14:13:14 +0200

    Tolto bt_test e aggiunta print a commandcenter
    La funzione di stampa è stata implementata dentro il pulsante di esportazione ordini. Il form degli ordini è generato in localmente alla subroutine del pulsante

---
+++ TokiDrift/CommandCenter.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CommandCenter
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CommandCenter))
+        Me.BtnAvviaOrdini = New System.Windows.Forms.Button()
+        Me.BtnFermaOrdini = New System.Windows.Forms.Button()
+        Me.BtnEsportaOrdini = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LblNumOrdini = New System.Windows.Forms.Label()
+        Me.LblStatusOrdini = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.LstUsers = New System.Windows.Forms.ListView()
+        Me.PrintOrder = New System.Drawing.Printing.PrintDocument()
+        Me.PrintOptions = New System.Windows.Forms.PrintDialog()
+        Me.SuspendLayout()
+        '
+        'BtnAvviaOrdini
+        '
+        Me.BtnAvviaOrdini.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnAvviaOrdini.Location = New System.Drawing.Point(77, 12)
+        Me.BtnAvviaOrdini.Name = "BtnAvviaOrdini"
+        Me.BtnAvviaOrdini.Size = New System.Drawing.Size(235, 30)
+        Me.BtnAvviaOrdini.TabIndex = 1
+        Me.BtnAvviaOrdini.Text = "(っ◔◡◔)っ ♥ Avvia ordini ♥"
+        Me.BtnAvviaOrdini.UseVisualStyleBackColor = True
+        '
+        'BtnFermaOrdini
+        '
+        Me.BtnFermaOrdini.Enabled = False
+        Me.BtnFermaOrdini.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnFermaOrdini.Location = New System.Drawing.Point(77, 48)
+        Me.BtnFermaOrdini.Name = "BtnFermaOrdini"
+        Me.BtnFermaOrdini.Size = New System.Drawing.Size(235, 30)
+        Me.BtnFermaOrdini.TabIndex = 2
+        Me.BtnFermaOrdini.Text = "(っ◔◡◔)っ ♥ Ferma ordini ♥"
+        Me.BtnFermaOrdini.UseVisualStyleBackColor = True
+        '
+        'BtnEsportaOrdini
+        '
+        Me.BtnEsportaOrdini.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnEsportaOrdini.Location = New System.Drawing.Point(77, 219)
+        Me.BtnEsportaOrdini.Name = "BtnEsportaOrdini"
+        Me.BtnEsportaOrdini.Size = New System.Drawing.Size(235, 30)
+        Me.BtnEsportaOrdini.TabIndex = 3
+        Me.BtnEsportaOrdini.Text = "E S P O R T A　（ラ芋往）"
+        Me.BtnEsportaOrdini.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Red
+        Me.Label1.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(76, 129)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(112, 15)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Ordini ricevuti"
+        '
+        'LblNumOrdini
+        '
+        Me.LblNumOrdini.AutoSize = True
+        Me.LblNumOrdini.BackColor = System.Drawing.Color.Red
+        Me.LblNumOrdini.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblNumOrdini.ForeColor = System.Drawing.Color.White
+        Me.LblNumOrdini.Location = New System.Drawing.Point(298, 129)
+        Me.LblNumOrdini.Name = "LblNumOrdini"
+        Me.LblNumOrdini.Size = New System.Drawing.Size(14, 15)
+        Me.LblNumOrdini.TabIndex = 5
+        Me.LblNumOrdini.Text = "0"
+        '
+        'LblStatusOrdini
+        '
+        Me.LblStatusOrdini.AutoSize = True
+        Me.LblStatusOrdini.BackColor = System.Drawing.Color.Gold
+        Me.LblStatusOrdini.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblStatusOrdini.ForeColor = System.Drawing.Color.White
+        Me.LblStatusOrdini.Location = New System.Drawing.Point(264, 105)
+        Me.LblStatusOrdini.Name = "LblStatusOrdini"
+        Me.LblStatusOrdini.Size = New System.Drawing.Size(49, 15)
+        Me.LblStatusOrdini.TabIndex = 7
+        Me.LblStatusOrdini.Text = "Chiusi"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Gold
+        Me.Label4.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(76, 105)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(91, 15)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Stato ordini"
+        '
+        'LstUsers
+        '
+        Me.LstUsers.BackColor = System.Drawing.Color.Pink
+        Me.LstUsers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.LstUsers.ForeColor = System.Drawing.Color.Teal
+        Me.LstUsers.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None
+        Me.LstUsers.HideSelection = False
+        Me.LstUsers.Location = New System.Drawing.Point(77, 148)
+        Me.LstUsers.Name = "LstUsers"
+        Me.LstUsers.Size = New System.Drawing.Size(235, 65)
+        Me.LstUsers.TabIndex = 8
+        Me.LstUsers.UseCompatibleStateImageBehavior = False
+        Me.LstUsers.View = System.Windows.Forms.View.List
+        '
+        'PrintOptions
+        '
+        Me.PrintOptions.AllowPrintToFile = False
+        Me.PrintOptions.Document = Me.PrintOrder
+        Me.PrintOptions.UseEXDialog = True
+        '
+        'CommandCenter
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.TokiDriftApplication.My.Resources.Resources.CommandCenterBkg
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(394, 261)
+        Me.Controls.Add(Me.LstUsers)
+        Me.Controls.Add(Me.LblStatusOrdini)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.LblNumOrdini)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnEsportaOrdini)
+        Me.Controls.Add(Me.BtnFermaOrdini)
+        Me.Controls.Add(Me.BtnAvviaOrdini)
+        Me.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "CommandCenter"
+        Me.Text = "(っ◔◡◔)っ 【 ﻿T O K I   D R I F T 】 (っ◔◡◔)っ"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnAvviaOrdini As Button
+	Friend WithEvents BtnFermaOrdini As Button
+	Friend WithEvents BtnEsportaOrdini As Button
+	Friend WithEvents Label1 As Label
+	Friend WithEvents LblNumOrdini As Label
+	Friend WithEvents LblStatusOrdini As Label
+	Friend WithEvents Label4 As Label
+	Friend WithEvents LstUsers As ListView
+	Friend WithEvents PrintOrder As Printing.PrintDocument
+	Friend WithEvents PrintOptions As PrintDialog
+End Class
---
+++ TokiDrift/CommandCenter.vb
@@ -0,0 +1,377 @@
+﻿Imports MQTTnet
+Imports MQTTnet.Client.Options
+Imports MQTTnet.Server
+Imports MQTTnet.Client.Receiving
+Imports MQTTnet.Client
+Imports System.Drawing.Printing
+
+Public Class CommandCenter
+  Implements IMqttApplicationMessageReceivedHandler
+  Private ReadOnly Factory As New MqttFactory
+  Private WithEvents MqttClient As MqttClient
+
+  Public lbl_Nomi_Points = New List(Of Point) From {
+    New Point(33, 332),
+    New Point(33, 351),
+    New Point(33, 369),
+    New Point(33, 388),
+    New Point(33, 407),
+    New Point(33, 426),
+    New Point(33, 445),
+    New Point(33, 464),
+    New Point(33, 483),
+    New Point(33, 502),
+    New Point(33, 521),
+    New Point(33, 540),
+    New Point(33, 559),
+    New Point(33, 578),
+    New Point(163, 332),
+    New Point(163, 351),
+    New Point(163, 369),
+    New Point(163, 388),
+    New Point(163, 407),
+    New Point(163, 426),
+    New Point(163, 445),
+    New Point(163, 464),
+    New Point(163, 483),
+    New Point(163, 502),
+    New Point(163, 521),
+    New Point(163, 540),
+    New Point(163, 559),
+    New Point(163, 578),
+    New Point(295, 332),
+    New Point(295, 351),
+    New Point(295, 369),
+    New Point(295, 388),
+    New Point(295, 407),
+    New Point(295, 426),
+    New Point(295, 445),
+    New Point(295, 464),
+    New Point(295, 483),
+    New Point(295, 502),
+    New Point(295, 521),
+    New Point(295, 540),
+    New Point(295, 559),
+    New Point(295, 578)
+  }
+  Public lbl_Qnt_Points = New List(Of Point) From {
+    New Point(101, 332),
+    New Point(101, 351),
+    New Point(101, 369),
+    New Point(101, 388),
+    New Point(101, 407),
+    New Point(101, 426),
+    New Point(101, 445),
+    New Point(101, 464),
+    New Point(101, 483),
+    New Point(101, 502),
+    New Point(101, 521),
+    New Point(101, 540),
+    New Point(101, 559),
+    New Point(101, 578),
+    New Point(231, 332),
+    New Point(231, 351),
+    New Point(231, 369),
+    New Point(231, 388),
+    New Point(231, 407),
+    New Point(231, 426),
+    New Point(231, 445),
+    New Point(231, 464),
+    New Point(231, 483),
+    New Point(231, 502),
+    New Point(231, 521),
+    New Point(231, 540),
+    New Point(231, 559),
+    New Point(231, 578),
+    New Point(363, 332),
+    New Point(363, 351),
+    New Point(363, 369),
+    New Point(363, 388),
+    New Point(363, 407),
+    New Point(363, 426),
+    New Point(363, 445),
+    New Point(363, 464),
+    New Point(363, 483),
+    New Point(363, 502),
+    New Point(363, 521),
+    New Point(363, 540),
+    New Point(363, 559),
+    New Point(363, 578)
+  }
+
+
+  '--- T O K I D R I F T | Constants -------------------------------------------------------------------------------------'
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Const MQTTROOT As String = "kronelab/tokidrift/"
+  Private Const MQTTHOST As String = "broker.hivemq.com"
+  Private Const MQTTUSER As String = ""
+  Private Const MQTTPASS As String = ""
+  Private Const MQTTPORT As UShort = 1883
+  Private Const MQTTQOFS As Protocol.MqttQualityOfServiceLevel = Protocol.MqttQualityOfServiceLevel.AtMostOnce
+  Private Const SEPTCHAR As String = "♥"
+
+
+  '--- T O K I D R I F T | Structures ------------------------------------------------------------------------------------'
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Structure OrdineStruct
+    Dim Nome As String
+    Dim Ordine As String
+  End Structure
+
+
+  '--- T O K I D R I F T | Variables -------------------------------------------------------------------------------------'
+  '-----------------------------------------------------------------------------------------------------------------------'
+  ReadOnly Ordini As New List(Of OrdineStruct)
+
+
+  '--- T O K I D R I F T | Private functions -----------------------------------------------------------------------------'
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Function SearchNameInList(ByVal name As String) As Short
+    Dim ret As Short = -1
+    Dim idx As UShort = 0
+    For Each ordine In Ordini
+      If ordine.Nome = name Then
+        ret = idx
+        Exit For
+      End If
+      idx += 1
+    Next
+    Return ret
+  End Function
+
+
+  '--- T O K I D R I F T | MQTT Service Functions ------------------------------------------------------------------------'
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Function HandleApplicationMessageReceivedAsync(eventArgs As MqttApplicationMessageReceivedEventArgs) As Task Implements IMqttApplicationMessageReceivedHandler.HandleApplicationMessageReceivedAsync
+    Dim payload As String()
+    Dim trusted_payload As String
+    Dim trusted_topic As String = Decrypt(eventArgs.ApplicationMessage.Topic, True)
+    Select Case trusted_topic
+      Case MQTTROOT
+        Dim recv As OrdineStruct
+        Dim idxv As Short
+        trusted_payload = Decrypt(System.Text.Encoding.UTF8.GetString(eventArgs.ApplicationMessage.Payload), False)
+        payload = trusted_payload.Split(SEPTCHAR)
+        recv.Nome = payload(0)
+        recv.Ordine = payload(1)
+        idxv = SearchNameInList(recv.Nome)
+        If idxv >= 0 Then
+          Ordini.RemoveAt(idxv)
+        End If
+        Ordini.Add(recv)
+        LblNumOrdini.Text = Ordini.Count
+        LstUsers.Clear()
+        For Each ordine As OrdineStruct In Ordini
+          Dim user As New ListViewItem(New String() {ordine.Nome})
+          LstUsers.Items.Add(user)
+        Next
+    End Select
+    Return Nothing
+  End Function
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Async Sub MQTTConnectToServer(id As String, uri As String, user As String, pwd As String, port As String)
+    Dim messageBuilder As New MqttClientOptionsBuilder
+    Dim options As New MqttClientOptions
+    Dim cancellationToken As Threading.CancellationToken
+    MqttClient = Factory.CreateMqttClient()
+    messageBuilder.WithClientId(id)
+    messageBuilder.WithCredentials(user, pwd)
+    messageBuilder.WithTcpServer(uri, CInt(port))
+    messageBuilder.WithCleanSession(True)
+    messageBuilder.Build()
+    messageBuilder.WithKeepAlivePeriod(TimeSpan.FromSeconds(60))
+    Try
+      Await MqttClient.ConnectAsync(messageBuilder.Build(), cancellationToken)
+    Catch ex As Exception
+      LblStatusOrdini.Text = "Chiusi"
+    Finally
+    End Try
+  End Sub
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Async Sub MQTTDisconnectFromServer()
+    Await MqttClient.DisconnectAsync()
+  End Sub
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Async Sub MQTTSubscribe(ByVal topic As String, qos As Protocol.MqttQualityOfServiceLevel)
+    Dim mqttTopicFilterBuilder As New TopicFilterBuilder
+    mqttTopicFilterBuilder.WithTopic(topic)
+    mqttTopicFilterBuilder.WithQualityOfServiceLevel(qos)
+    mqttTopicFilterBuilder.Build()
+    Try
+      Await MqttClient.SubscribeAsync(mqttTopicFilterBuilder.Build())
+    Catch ex As Exception
+      LblStatusOrdini.Text = "Chiusi"
+    Finally
+      LblStatusOrdini.Text = "Aperti"
+      BtnAvviaOrdini.Enabled = False
+      BtnFermaOrdini.Enabled = True
+    End Try
+  End Sub
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Function Connect(ByVal user As String) As Boolean
+    Dim timeout As Date = Date.Now
+    timeout = timeout.AddSeconds(10)
+    MQTTConnectToServer(user, MQTTHOST, MQTTUSER, MQTTPASS, MQTTPORT)
+    MqttClient.ApplicationMessageReceivedHandler = Me
+    While Not MqttClient.IsConnected
+      If Date.Now >= timeout Then
+        LblStatusOrdini.Text = "Chiusi"
+        Return False
+      End If
+    End While
+    Return True
+  End Function
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Sub Disconnect()
+    MQTTDisconnectFromServer()
+    While MqttClient.IsConnected
+
+    End While
+  End Sub
+
+
+  '--- T O K I D R I F T | Decryption ------------------------------------------------------------------------------------'
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Function Encrypt(ByVal encodedata As String, ByVal optopic As Boolean) As String
+    Dim wrapper As New Simple3Des("Sashimi69")
+    Dim cipherText As String = wrapper.EncryptData(encodedata)
+    If optopic Then
+      Dim strdebug As String = cipherText.Replace("+", "!").Replace("/", "?")
+      Return strdebug
+    Else
+      Return cipherText
+    End If
+  End Function
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Function Decrypt(ByVal decodedata As String, ByVal optopic As Boolean) As String
+    Dim wrapper As New Simple3Des("Sashimi69")
+    Try
+      If optopic Then
+        Return wrapper.DecryptData(decodedata.Replace("!", "+").Replace("?", "/"))
+      Else
+        Return wrapper.DecryptData(decodedata)
+      End If
+    Catch ex As Security.Cryptography.CryptographicException
+      Return "crypt err"
+    End Try
+  End Function
+
+
+  '--- T O K I D R I F T | User Interface --------------------------------------------------------------------------------'
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Sub CommandCenter_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    CheckForIllegalCrossThreadCalls = False
+    Ordini.Clear()
+    LstUsers.Clear()
+  End Sub
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Sub BtnAvviaOrdini_Click(sender As Object, e As EventArgs) Handles BtnAvviaOrdini.Click
+    If Connect("MasterSashimi") Then
+      MQTTSubscribe(Encrypt(MQTTROOT, True), MQTTQOFS)
+    End If
+  End Sub
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Sub BtnFermaOrdini_Click(sender As Object, e As EventArgs) Handles BtnFermaOrdini.Click
+    Disconnect()
+    BtnFermaOrdini.Enabled = False
+    BtnAvviaOrdini.Enabled = True
+    LblStatusOrdini.Text = "Chiusi"
+  End Sub
+  '-----------------------------------------------------------------------------------------------------------------------'
+  Private Sub BtnEsportaOrdini_Click(sender As Object, e As EventArgs) Handles BtnEsportaOrdini.Click
+    Dim progCell As Integer = 0
+    Dim Charfont As New Drawing.Font("Calibri", 12, FontStyle.Bold)
+    Dim frmNew As New Form With {
+      .Width = 410,
+      .Height = 680,
+      .BackColor = Drawing.Color.Black,
+      .FormBorderStyle = FormBorderStyle.FixedSingle,
+      .Icon = TokiDrift.ActiveForm.Icon,
+      .Text = "Ordine definitivo - お前はもう死んでいる"
+    }
+    Dim tmp_pb As New PictureBox With {
+      .Location = New Point(0, 0),
+      .Width = 395,
+      .Height = 601,
+      .BackgroundImageLayout = ImageLayout.Center
+    }
+    Dim img As Bitmap = My.Resources.ListaOrdini
+    Dim printbut As New Button With {
+      .Location = New Point(100, 603),
+      .Width = 200,
+      .Height = 35,
+      .Text = "Stampa",
+      .Font = Charfont,
+      .BackColor = Drawing.SystemColors.Control
+    }
+    frmNew.Controls.Add(tmp_pb)
+    frmNew.Controls.Add(printbut)
+    AddHandler printbut.Click, Sub(s As Object, ev As EventArgs)
+                                 If PrintOptions.ShowDialog() = DialogResult.OK Then
+                                   PrintOrder.PrinterSettings = PrintOptions.PrinterSettings
+                                   TokiDrift.PrintImage = img
+                                   TokiDrift.PrintImage.SetResolution(1200, 1200)
+                                   AddHandler PrintOrder.PrintPage, AddressOf PrintDocument_PrintPage
+                                   PrintOrder.DocumentName = "Lista ordini Toki"
+                                   PrintOrder.Print()
+                                 End If
+                               End Sub
+    Dim ListaUtenti As New List(Of String)
+    Dim ListaPiatti As New List(Of String)
+    Dim ListaPortate As New List(Of String)
+    Dim piatto As String()
+    Dim tstr As String()
+    Dim DT As String = Date.Now().ToString
+    Dim file As IO.StreamWriter
+
+    file = My.Computer.FileSystem.OpenTextFileWriter("Ordine.txt", True)
+    file.WriteLine(DT)
+    file.WriteLine("O R D I N I   S I N G O L I")
+
+    If Ordini.Count > 0 Then
+      tstr = Ordini(0).Ordine.Split(";")
+      For i = 0 To tstr.Count - 2
+        piatto = tstr(i).Split(".")
+        ListaPiatti.Add(piatto(0))
+        ListaPortate.Add("0")
+      Next
+      For Each ordine In Ordini
+        file.WriteLine("> " & ordine.Nome)
+        tstr = ordine.Ordine.Split(";")                 ' separazione coppie piatto.portata
+        For i = 0 To tstr.Count - 2
+          Dim tempint As UInt16
+          piatto = tstr(i).Split(".")                   ' separazione piatto (piatto(0)) e portate (piatto(1))
+          tempint = Convert.ToUInt16(ListaPortate(i))   ' leggo il numero attuale di portate
+          tempint += Convert.ToUInt16(piatto(1))        ' aggiungo il numero di portate alla somma
+          ListaPortate(i) = tempint.ToString            ' converto la nuova somma portate in stringa
+          If piatto(1) <> 0 Then
+            file.WriteLine("  " & piatto(0) & " " & piatto(1))
+          End If
+        Next
+      Next
+      file.WriteLine("O R D I N E   F I N A L E")
+      Dim index As UShort = 0
+      For Each lst In ListaPiatti
+        If ListaPortate(index) <> 0 Then
+          file.WriteLine(ListaPiatti(index) & " " & ListaPortate(index))
+          Using g = Graphics.FromImage(img)
+            g.DrawString(ListaPiatti(index).ToString, Charfont, Brushes.Black, lbl_Nomi_Points(progCell))
+            g.DrawString(ListaPortate(index).ToString, Charfont, Brushes.Black, lbl_Qnt_Points(progCell))
+          End Using
+          progCell += 1
+        End If
+        index += 1
+      Next
+      MsgBox("Ordine esportato")
+      file.Close()
+      tmp_pb.Image = img
+      frmNew.ShowDialog()
+    End If
+  End Sub
+
+  Private Sub PrintDocument_PrintPage(ByVal sender As Object, ByVal e As PrintPageEventArgs)
+    e.Graphics.RenderingOrigin = New Point(e.MarginBounds.Left, e.MarginBounds.Top)
+    e.Graphics.DrawImage(TokiDrift.PrintImage, New Rectangle(0, 0, 395, 601))
+    e.HasMorePages = False
+  End Sub
+End Class
